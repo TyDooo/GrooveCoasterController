f98d06a614df0826a6745daccb37888053bd9758
TyDooo <tydooo@fastmail.com>
2023-04-17 21:26:20 +0200

    feat(firmware): :sparkles: Add debouncing for buttons and dpad

--- firmware/src/PiCoaster2040.c
+++ firmware/src/PiCoaster2040.c
@@ -1,27 +1,15 @@
 #include "PiCoaster2040.h"
-#include "hardware/gpio.h"
-#include "pinmap.h"
 #include "tusb.h"
 #include "usb/usb_descriptors.h"
+#include "buttons.h"
 
-void init_button_gpio(uint gpio);
-static uint8_t get_gamepad_hat(bool up, bool down, bool left, bool right);
+hid_gamepad_report_t report = {0};
 
 void setup()
 {
     tud_init(BOARD_TUD_RHPORT);
 
-    init_button_gpio(BOOSTER_LEFT_UP);
-    init_button_gpio(BOOSTER_LEFT_DOWN);
-    init_button_gpio(BOOSTER_LEFT_LEFT);
-    init_button_gpio(BOOSTER_LEFT_RIGHT);
-    init_button_gpio(BOOSTER_LEFT_BTN);
-
-    init_button_gpio(BOOSTER_RIGHT_UP);
-    init_button_gpio(BOOSTER_RIGHT_DOWN);
-    init_button_gpio(BOOSTER_RIGHT_LEFT);
-    init_button_gpio(BOOSTER_RIGHT_RIGHT);
-    init_button_gpio(BOOSTER_RIGHT_BTN);
+    init_buttons();
 }
 
 void update()
@@ -31,25 +19,8 @@ void update()
     if (!tud_ready())
         return;
 
-    uint32_t values = ~gpio_get_all();
-    hid_gamepad_report_t report = {0};
-
-    // Read the microswitch states from the input pins
-    bool up = values & (1u << BOOSTER_LEFT_UP);
-    bool down = values & (1u << BOOSTER_LEFT_DOWN);
-    bool left = values & (1u << BOOSTER_LEFT_LEFT);
-    bool right = values & (1u << BOOSTER_LEFT_RIGHT);
-
-    // get the hat direction based on the microswitch states above
-    report.hat = get_gamepad_hat(up, down, left, right);
-
-    // Set gamepad button states based on the values of the input pins
-    report.buttons = 0 | ((values & (1u << BOOSTER_RIGHT_UP)) ? GAMEPAD_BUTTON_A : 0) |
-                     ((values & (1u << BOOSTER_RIGHT_DOWN)) ? GAMEPAD_BUTTON_B : 0) |
-                     ((values & (1u << BOOSTER_RIGHT_LEFT)) ? GAMEPAD_BUTTON_X : 0) |
-                     ((values & (1u << BOOSTER_RIGHT_RIGHT)) ? GAMEPAD_BUTTON_Y : 0) |
-                     ((values & (1u << BOOSTER_RIGHT_BTN)) ? GAMEPAD_BUTTON_TR : 0) |
-                     ((values & (1u << BOOSTER_LEFT_BTN)) ? GAMEPAD_BUTTON_TL : 0);
+    update_dpad(&report);
+    update_buttons(&report);
 
     tud_hid_report(REPORT_ID_GAMEPAD, &report, sizeof(report));
 
@@ -82,34 +53,27 @@ uint16_t tud_hid_get_report_cb(uint8_t instance, uint8_t report_id, hid_report_t
     return 0;
 }
 
-void init_button_gpio(uint gpio)
-{
-    gpio_init(gpio);
-    gpio_set_dir(gpio, GPIO_IN);
-    gpio_pull_up(gpio);
-}
-
-static uint8_t get_gamepad_hat(bool up, bool down, bool left, bool right)
-{
-    switch ((up << 3) | (down << 2) | (left << 1) | right)
-    {
-    case 0b1000:
-        return GAMEPAD_HAT_UP;
-    case 0b0100:
-        return GAMEPAD_HAT_DOWN;
-    case 0b0010:
-        return GAMEPAD_HAT_LEFT;
-    case 0b0001:
-        return GAMEPAD_HAT_RIGHT;
-    case 0b1010:
-        return GAMEPAD_HAT_UP_LEFT;
-    case 0b1001:
-        return GAMEPAD_HAT_UP_RIGHT;
-    case 0b0110:
-        return GAMEPAD_HAT_DOWN_LEFT;
-    case 0b0101:
-        return GAMEPAD_HAT_DOWN_RIGHT;
-    default:
-        return GAMEPAD_HAT_CENTERED;
-    }
-}
+// static uint8_t get_gamepad_hat(bool up, bool down, bool left, bool right)
+// {
+//     switch ((up << 3) | (down << 2) | (left << 1) | right)
+//     {
+//     case 0b1000:
+//         return GAMEPAD_HAT_UP;
+//     case 0b0100:
+//         return GAMEPAD_HAT_DOWN;
+//     case 0b0010:
+//         return GAMEPAD_HAT_LEFT;
+//     case 0b0001:
+//         return GAMEPAD_HAT_RIGHT;
+//     case 0b1010:
+//         return GAMEPAD_HAT_UP_LEFT;
+//     case 0b1001:
+//         return GAMEPAD_HAT_UP_RIGHT;
+//     case 0b0110:
+//         return GAMEPAD_HAT_DOWN_LEFT;
+//     case 0b0101:
+//         return GAMEPAD_HAT_DOWN_RIGHT;
+//     default:
+//         return GAMEPAD_HAT_CENTERED;
+//     }
+// }
