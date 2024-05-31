8fe3267911b476dd3bd54d121cdae7f97f8c2b5e
Tygo Driessen <tygo@driessen.family>
2024-05-31 20:00:48 +0200

    chore: Update flake configuration and dependencies
    Signed-off-by: Tygo Driessen <tygo@driessen.family>

--- flake.nix
+++ flake.nix
@@ -1,45 +1,59 @@
 {
-  description = "A very basic flake";
+  description = "Description for the project";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
-    flake-utils.url = "github:numtide/flake-utils";
+    devshell.url = "github:numtide/devshell";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        pico-sdk151 = with pkgs;
-          (pico-sdk.overrideAttrs (o: rec {
-            pname = "pico-sdk";
-            version = "1.5.1";
-            src = fetchFromGitHub {
-              fetchSubmodules = true;
-              owner = "raspberrypi";
-              repo = pname;
-              rev = version;
-              sha256 = "sha256-GY5jjJzaENL3ftuU5KpEZAmEZgyFRtLwGVg3W1e/4Ho=";
-            };
-          }));
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [
+        inputs.devshell.flakeModule
+      ];
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        ...
+      }: let
+        # https://github.com/NixOS/nixpkgs/issues/175297
+        pico-sdk151 = with pkgs; (pico-sdk.overrideAttrs (o: rec {
+          pname = "pico-sdk";
+          version = "1.5.1";
+          src = fetchFromGitHub {
+            fetchSubmodules = true;
+            owner = "raspberrypi";
+            repo = pname;
+            rev = version;
+            sha256 = "sha256-GY5jjJzaENL3ftuU5KpEZAmEZgyFRtLwGVg3W1e/4Ho=";
+          };
+        }));
       in {
-        devShells.default = pkgs.mkShell rec {
-          name = "GrooveCoasterController";
-
-          packages = with pkgs; [
-            python3
-            cmake
-            clang-tools
-            gcc-arm-embedded
+        devshells.default = {
+          packages = [
             pico-sdk151
+            pkgs.cmake
+            pkgs.gcc
+            pkgs.gcc-arm-embedded
+            pkgs.gnumake
+            pkgs.python3
+            pkgs.openocd
+            pkgs.gdb
+            pkgs.picotool
           ];
 
-          shellHook = let icon = "f121";
-          in ''
-            export PS1="$(echo -e '\u${icon}') {\[$(tput sgr0)\]\[\033[38;5;228m\]\w\[$(tput sgr0)\]\[\033[38;5;15m\]} (${name}) \\$ \[$(tput sgr0)\]"
-            export PICO_SDK_PATH="${pico-sdk151}/lib/pico-sdk"
-          '';
+          env = [
+            {
+              name = "PICO_SDK_PATH";
+              value = "${pico-sdk151}/lib/pico-sdk";
+            }
+          ];
         };
-      });
-
+      };
+    };
 }
